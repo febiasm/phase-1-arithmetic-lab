67127277f48df73b15cee44ae2881baca20f4e21
febiasm <trevorfebias@gmail.com>
2022-11-23 22:00:22 +0300

    done

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 
 // Write your code here
+const  num1 = 15.5
+const num2 = 4
+const num3 = 24
+const num4 =10
+const multiply = num1 * num2
+    
+const random = Math.floor(Math.random()*10)+1;
+
+const mod = num3% num4
+
+
+const max = Math.max(1 , 10 ,15 ,20);
+
+
